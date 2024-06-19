33db54baccfafd72b26eaf4583e7910502d30634
mark-abraham-dev <mark.a.veron.1989@gmail.com>
2024-06-18 23:03:36 -0500

    basic components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import Dropdown from "./components/Dropdown/Dropdown";
+import SendButtonIcon from "./assets/icons/send-button.svg";
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [userRole, setUserRole] = useState<string>("Admin");
+
+  const handleChange = (newUserRole: string) => {
+    setUserRole(newUserRole);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Dropdown userRole={userRole} onChange={handleChange} />
+      <div className="welcome-box">
+        <p className="welcome-top-message">Hello, Dani</p>
+        <p className="welcome-bottom-message">Just a quick check in</p>
+      </div>
+      <div className="chat-box">
+        <input
+          type="text"
+          placeholder="Chat with CynchAI..."
+          className="chat-input"
+        />
+        <img src={SendButtonIcon} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/consts.ts
@@ -0,0 +1,1 @@
+export const USER_ROLES: string[] = ["Admin", "User", "Guest"];
